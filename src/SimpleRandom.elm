2ec3dd994a988f52e841fd5c447366afd4d2fc47
Mark <bitterjug@gmail.com>
2018-06-09 19:58:17 +0100

    Add map2 and apply

--- src/SimpleRandom.elm
+++ src/SimpleRandom.elm
@@ -30,10 +30,23 @@ type alias Generator a =
     Seed -> ( a, Seed )
 
 
+run : Seed -> Generator a -> a
+run s0 g =
+    g s0 |> Tuple.first
+
+
 map : (a -> b) -> Generator a -> Generator b
 map f gen =
     gen
         >> Tuple.mapFirst f
+
+
+andThen : (a -> Generator b) -> Generator a -> Generator b
+andThen f gen =
+    -- gen >> uncurry f
+    \seed1 ->
+        gen seed1
+            |> (\( aVal, seed2 ) -> f aVal seed2)
 
 
 {-| We can generate int values easily
@@ -55,6 +68,41 @@ bool : Generator Bool
 bool =
     next
         |> map ((>) 0.5)
+
+
+pair : Generator a -> Generator b -> Generator ( a, b )
+pair =
+    map2 (,)
+
+
+pair_ : Generator a -> Generator b -> Generator ( a, b )
+pair_ ga gb =
+    lift (,)
+        |> apply ga
+        |> apply gb
+
+
+map2 : (a -> b -> c) -> Generator a -> Generator b -> Generator c
+map2 f genA genB =
+    \s0 ->
+        let
+            ( aVal, s1 ) =
+                genA s0
+
+            ( bVal, s2 ) =
+                genB s1
+        in
+        ( f aVal bVal, s2 )
+
+
+lift : a -> Generator a
+lift aVal =
+    \seed -> ( aVal, seed )
+
+
+apply : Generator a -> Generator (a -> b) -> Generator b
+apply =
+    map2 (\a f -> f a)
 
 
 list : Int -> Generator a -> Generator (List a)
